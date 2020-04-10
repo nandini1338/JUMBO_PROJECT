d10482d287a8cf6619b145a08dd9d282668e16e1
nandini1338 <nandinikanojia999@gmail.com>
2020-04-09 20:34:15 -0700

    manager commit 5

--- app/src/main/java/com/example/employeemanagementsystem/Manager.java
+++ app/src/main/java/com/example/employeemanagementsystem/Manager.java
@@ -12,29 +12,36 @@ public class Manager extends Employee {
     Car car = new Car();
     MotorCycle motorcycle = new MotorCycle();
 
-    Manager()
-    {
+    Manager() {
         nbClients = 0;
     }
 
-    public void get(EditText txtfname,EditText txtlname,EditText txtbyer,EditText txtmsalary,EditText txtorate,EditText txtcno, EditText txtcartype,EditText txtvmodel,EditText txtplateno,String txtsidecar)
-    {
-        super.get(txtfname,txtlname,txtbyer,txtmsalary,txtorate);
-        if(Vehicle.category.equals("Car"))
-        {
-            car.get(txtcartype,txtvmodel,txtplateno);
-        }
-        else if(Vehicle.category.equals("Motor Cycle"))
-        {
-            motorcycle.get(txtvmodel,txtplateno,txtsidecar);
+    public void get(EditText txtfname, EditText txtlname, EditText txtbyer, EditText txtmsalary, EditText txtorate, EditText txtcno, EditText txtcartype, EditText txtvmodel, EditText txtplateno, String txtsidecar) {
+        super.get(txtfname, txtlname, txtbyer, txtmsalary, txtorate);
+        if (Vehicle.category.equals("Car")) {
+            car.get(txtcartype, txtvmodel, txtplateno);
+        } else if (Vehicle.category.equals("Motor Cycle")) {
+            motorcycle.get(txtvmodel, txtplateno, txtsidecar);
         }
         nbClients = Integer.parseInt(txtcno.getText().toString());
     }
 
-    public void activity_store(Intent intent)
-    {
+    public void activity_store(Intent intent) {
         super.activity_store(intent);
         car.activity_store(intent);
         motorcycle.activity_store(intent);
-        intent.putExtra("Performance","He/She has bought "+nbClients+" clients");
+        intent.putExtra("Performance", "He/She has bought " + nbClients + " clients");
     }
+
+    public void set(Intent intent, Bundle bundle, TextView txtname, TextView txtage, TextView txtaIncome, TextView txtrate, TextView txtperformance, TextView txtmodel, TextView txtplate, TextView txtvtype, TextView txtcolor, TextView txttype) {
+        super.set(intent, bundle, txtname, txtage, txtaIncome, txtrate);
+        if (Vehicle.category.equals("Car")) {
+            car.set(intent, bundle, txtmodel, txtplate, txtvtype, txtcolor, txttype);
+        } else if (Vehicle.category.equals("Motor Cycle")) {
+            motorcycle.set(intent, bundle, txtmodel, txtplate, txtvtype, txtcolor, txttype);
+        }
+        String tempperformance = bundle.getString("Performance");
+        txtperformance.setText(tempperformance);
+    }
+
+}
