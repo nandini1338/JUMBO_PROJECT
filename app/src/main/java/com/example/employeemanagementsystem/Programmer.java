5f506bb8c37d69473015b7737ca414d41190bcc9
nandini1338 <nandinikanojia999@gmail.com>
2020-04-09 20:39:00 -0700

    programmer commit 6

---
+++ app/src/main/java/com/example/employeemanagementsystem/Programmer.java
@@ -0,0 +1,21 @@
+package com.example.employeemanagementsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Programmer extends Employee {
+
+    private int nbProjects;
+    Car car = new Car();
+    MotorCycle motorcycle = new MotorCycle();
+
+
+    Programmer()
+    {
+        nbProjects = 0;
+    }
+
+
+}
