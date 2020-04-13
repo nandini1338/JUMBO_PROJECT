cd9773c566548bb15f86535fca612730bcea7258
sukhdeeptt <sukhsaab2012@gmail.com>
2020-04-12 22:31:38 -0400

    MotorCycle class

---
+++ app/src/main/java/com/example/employeemanagementsystem/MotorCycle.java
@@ -0,0 +1,18 @@
+package com.example.employeemanagementsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeoutException;
+
+public class MotorCycle extends Vehicle {
+    private String sideCar;
+
+    MotorCycle()
+    {
+        sideCar = "";
+    }
