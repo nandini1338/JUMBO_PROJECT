a16c11fcd1e5727fb8e0333ee70bd45da88e29cf
gauravchopra1168 <60721767+gauravchopra1168@users.noreply.github.com>
2020-04-13 10:19:55 -0400

    Changes in MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.employeemanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Programmer programmer = new Programmer();
+    Tester tester = new Tester();
+    Manager manager = new Manager();
+    String txtsidecar = new String();
+    String Vcategory = new String();
+    String Vcolor = new String();
+    DataHelper myDB ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
